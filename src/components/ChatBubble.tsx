49bdc18245ac6631cd1148761c82477c630cfa03
saba8814 <ejub1946@outlook.com>
2023-01-19 18:24:04 +0100

    fixed-bad-state-crash

--- src/components/ChatBubble.tsx
+++ src/components/ChatBubble.tsx
@@ -12,14 +12,14 @@ interface Props {
 const ChatBubble = ({ ownMessage, address, message, timestamp,decryptionKey }: Props) => {
   const bubblePosition = ownMessage ? "right" : "left";
   const formatDate = new Date(parseInt(timestamp)*1000).toString();
-  const decryptedMessage = crypto.AES.decrypt(message,decryptionKey).toString(crypto.enc.Utf8);
+  
   return (
     <div className="chat_row">
       {!ownMessage && <small className="address-text">Sent by: {address} on {formatDate}</small>}
       <div className={["chat_bubble", bubblePosition].join(" ")}>
         <div className={["chat_message", bubblePosition].join(" ")}>
           {ownMessage}
-          {decryptedMessage.length===0 ? message.substring(0,45) : decryptedMessage}
+          {crypto.AES.decrypt(message,decryptionKey).toString(crypto.enc.Utf8).length===0 ? message.substring(0,45) : crypto.AES.decrypt(message,decryptionKey).toString(crypto.enc.Utf8)}
         </div>
       </div>
     </div>
